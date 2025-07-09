e66639ba56e6df1f5f0a952ee30e9332344e44fe
Suleiman Osman <suleosman73@gmail.com>
2025-07-09 21:24:37 +0300

    add svg logo

--- components/home/HeroSection.jsx
+++ components/home/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from 'next/image';
 import HeroImage from '@/public/assets/white.jpg';
-import logo from '../../public/assets/logo.png';
+import logo from '../../public/assets/hero-logo.svg';
 
 const HeroSection = () => {
     return (
