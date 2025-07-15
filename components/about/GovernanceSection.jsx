92853768bc12d4865c5160f172d457e9882d7bdc
Suleiman Osman <suleosman73@gmail.com>
2025-07-15 10:54:12 +0300

    added mail

--- components/about/GovernanceSection.jsx
+++ components/about/GovernanceSection.jsx
@@ -60,11 +60,14 @@ const GovernanceSection = () => {
                                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
                             </svg>
                         </div>
-                        <p className={`text-gray-600 transition-all duration-500 ease-out delay-400 ${
-                            cardVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
-                        }`}>
+                        <a
+                            href="mailto:info@handselconsultancy.co.uk"
+                            className={`text-gray-600 hover:text-mainColor transition-colors duration-300 underline decoration-transparent hover:decoration-mainColor underline-offset-2 ${
+                                cardVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+                            }`}
+                        >
                             info@handselconsultancy.co.uk
-                        </p>
+                        </a>
                     </div>
                 </div>
             </div>
